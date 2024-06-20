c0b132d0b303981ee8fded5bf698143211ddcb5c
Rami Hsasna <ramihsasna34@gmail.com>
2024-06-20 17:32:34 +0100

    Introduced two new functions :
    -createGrid which creates a grid with a given number of boxes on each side
    and calls for the changeColor function
    -changeColor which changes the color of the cell on mouse hover

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-window.onload = () => {
-  const grid = document.getElementById("grid-container");
+var container = document.querySelector(".container");
+var button = document.querySelector("#clearButton");
 
-  if (grid) {
-    function createGrid() {
-      for (let i = 0; i < 16; i++) {
-        const cell = document.createElement("div");
-        cell.classList.add("grid-item");
-        cell.textContent = i;
-        grid.appendChild(cell);
-      }
+function addDiv(numberOfDivsToCreate) {
+  var gridCellDimensions = (600 / numberOfDivsToCreate - 2).toFixed(2);
+  var gridSize = Math.pow(numberOfDivsToCreate, 2);
+
+  // create grid squares & add to container
+  for (var rows = 0; rows < gridSize; rows++) {
+    for (var columns = 0; columns < gridSize; columns++) {
+      var newDiv = document.createElement("div");
+      container.appendChild(newDiv);
+      newDiv.classList.add("grid");
+      newDiv.style.height = gridCellDimensions + "px";
+      newDiv.style.width = gridCellDimensions + "px";
+      newDiv.style.border = "1px solid black";
+      gridSize--;
     }
-    createGrid();
-  } else {
-    console.error('Element with id "grid-container" does not exist');
   }
-};
+  var gridCells = document.querySelectorAll(".grid");
+  gridCells.forEach((cell) => cell.addEventListener("mouseover", changeColor));
+  console.log(gridCellDimensions);
+  console.log("Grid has been created!!");
+}
+
+function changeColor() {
+  this.style.backgroundColor = "#000000";
+}
+
+window.onload = addDiv(16);
